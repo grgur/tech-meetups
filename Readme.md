4f4f40b5d82eb1a9a645cd01831433651271e7c3
Grgur Grisogono <grgur@grgur.com>
2015-08-12 20:58:04 +0200

    Initial setup

---
+++ Readme.md
@@ -0,0 +1,6 @@
+```
+jspm init .
+jspm install react
+jspm install redux=github:gaearon/redux@v1.0.0-rc
+jspm resolve --only npm:core-js@1.0.1
+```
---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let {Component} = React;
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Universal App Example</h1>
+            </div>
+        );
+    }
+}
