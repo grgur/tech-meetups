db8bdcadf8c98708ca4370973a0992cf5a6c340b
Grgur Grisogono <grgur@grgur.com>
2015-08-13 21:38:33 +0200

    Spread your wings

--- app/reducers/VolumeLevels.js
+++ app/reducers/VolumeLevels.js
@@ -8,6 +8,7 @@ export default function volumeLevels(state = initialState, action) {
   switch (action.type) {
   case CHANGE_VOLUME:
     return {
+      ...state,
       level: action.level,
     };
 
