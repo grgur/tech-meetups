4082623859d41e00a13b8d7934c8880e8f2d02d7
Grgur Grisogono <grgur@grgur.com>
2015-08-14 11:26:29 +0200

    Removed JSPM, upgraded to react 0.14 beta 3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './app/App';
 
-React.render(
+ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
