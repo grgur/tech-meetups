8ec3163f20570d0f47eded402d78e767bbf071bd
Grgur Grisogono <grgur@grgur.com>
2015-08-14 17:11:21 +0200

    proptypes

--- app/slider/SliderContainer.js
+++ app/slider/SliderContainer.js
@@ -4,12 +4,17 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as VolumeActions from '../actions/VolumeActions';
 
-const { Component } = React;
+const { Component, PropTypes } = React;
 
 @connect(state => ({
   volume: state.volumelevels
 }))
 export default class SliderContainer extends Component {
+  static propTypes = {
+    volume: PropTypes.object,
+    dispatch: PropTypes.func,
+  };
+
   render() {
     const { volume, dispatch } = this.props;
     return (
