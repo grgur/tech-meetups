c575c80ec509ef4490884089133c0202a67b534a
Grgur Grisogono <grgur@grgur.com>
2015-08-14 17:31:41 +0200

    Make sure it's a number

--- app/actions/VolumeActions.js
+++ app/actions/VolumeActions.js
@@ -1,7 +1,7 @@
 import { CHANGE_VOLUME } from '../constants/VolumeTypes';
 
 export function onSliderChangeVolume(ev) {
-  const level = ev.target.value;
+  const level = parseInt(ev.target.value, 10);
 
   return {
     type: CHANGE_VOLUME,
