6d98de91bfb7a95e6b8d3ad659812a11c8d972aa
Grgur Grisogono <grgur@grgur.com>
2015-08-22 00:26:40 +0200

    Removed obsolete components and containers

--- app/App.js
+++ app/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import store from './stores';
-import SliderContainer from './slider/SliderContainer';
-import DisplayContainer from './display/DisplayContainer';
 import MeetupList from './view/meetup/MeetupList';
 
 const { Component } = React;
@@ -13,13 +11,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Universal App Example</h1>
+        <h1>Universal App Example - Tech Meetups</h1>
         <Provider store={store}>
-          {() => <div>
-                  <SliderContainer />
-                  <DisplayContainer />
-                  <MeetupList />
-                </div>
+          {() => <MeetupList />
           }
         </Provider>
         <DebugPanel top right bottom>
