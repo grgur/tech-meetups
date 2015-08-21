c28b6a685b0ddf90eb6f7137defadbbf57ede005
Grgur Grisogono <grgur@grgur.com>
2015-08-21 10:33:30 +0200

    Action fire action

--- app/actions/Geolocation.js
+++ app/actions/Geolocation.js
@@ -1,18 +1,23 @@
 import { RECEIVE_LOCATION } from '../constants/Types';
 import { defaultPosition } from '../constants/Geo';
+import { fetchMeetupGroups } from './Meetup';
 import store from '../stores';
 
 function requestGeolocation() {
   return function(dispatch) {
     navigator.geolocation.getCurrentPosition(function(position) {
+      const { latitude, longitude } = position.coords;
+
       dispatch({
         type: RECEIVE_LOCATION,
         coords: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+          latitude: latitude,
+          longitude: longitude,
           inProgress: false,
         },
       });
+
+      dispatch(fetchMeetupGroups({latitude, longitude}));
     });
   };
 }
